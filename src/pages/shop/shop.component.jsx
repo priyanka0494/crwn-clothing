a0c0b449f0d7d5a74acea4a3d2a35a7490d2247d
Priyanka Chavan <picky.4494@gmail.com>
2020-12-03 17:31:51 +0530

    shop-component minor changes

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
 
 const ShopPage = ({ match }) => {
 	return (
 		<div className='shop-page'>
-			<Route exac path={`${match.path}`} component={CollectionsOverview} />
+			<Route exact path={`${match.path}`} component={CollectionsOverview} />
 			<Route path={`${match.path}/:collectionId`} component={CollectionPage} />
 		</div>
 	);
