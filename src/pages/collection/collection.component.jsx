7c722c1469f498f5ab5ed6ec205476c0b478d0ac
Priyanka Chavan <picky.4494@gmail.com>
2020-12-07 16:09:33 +0530

    resolving small bugs

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -5,7 +5,7 @@ import CollectionItem from '../../components/collection-item/collection-item.com
 import './collection.styles.scss';
 
 const CollectionPage = ({ collection }) => {
-	console.log("Collection", collection);
+	const { title, items } = collection
 	return (
 		<div className='collection-page'>
 			<h2 className='title'>{title}</h2>
